1d70e7bacd1907a77ea86c77deaf52fdb6d61a6d
yaqiangbi <yaqiangbi@wholeeprime.com>
2021-11-02 17:46:46 +0800

    feat:改版

---
+++ IOSPackage.groovy
@@ -0,0 +1,12 @@
+def call(Object WorkflowScript) {
+    pipeline {
+        agent any 
+        stages {
+            stage('Stage 1') {
+                steps {
+                    echo 'Hello world!' 
+                }
+            }
+        }
+    }
+}
